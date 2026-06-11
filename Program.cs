101286736dabb219e6403535050bd10cdc2aefa0
Xander <fehsenfeld.xander@gmail.com>
2026-06-11 10:43:24 -0700

    Initialize mongo db connections

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Driver;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+var connectionString = Environment.GetEnvironmentVariable("MONGODB_URI");
+if (connectionString == null)
+{
+    Console.WriteLine("You must set your 'MONGODB_URI' environment variable. To learn how to set it, see https://www.mongodb.com/docs/drivers/csharp/current/get-started/create-connection-string");
+    Environment.Exit(0);
+}
+var client = new MongoClient(connectionString);
+var collection = client.GetDatabase("sample_mflix").GetCollection<BsonDocument>("movies");
+var filter = Builders<BsonDocument>.Filter.Eq("title", "Back to the Future");
+var document = collection.Find(filter).First();
+Console.WriteLine(document.ToJson(new JsonWriterSettings { Indent = true }));
